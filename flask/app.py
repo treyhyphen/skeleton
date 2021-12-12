5f0ebe5ef3262b8d0fe2c3223125819364577c1f
Trey Allen <trey.allen@gmail.com>
2021-12-12 14:49:47 -0500

    Shuffle some things around to make more sense.

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,5 @@
 """Run the jewels."""
 from application import app
-from dotenv import load_dotenv
-from os import path
 
 if __name__ == "__main__":
-    if path.exists(".env"):
-        load_dotenv('.env', verbose=True)
-
     app.run()
--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -6,7 +6,10 @@ Returns:
 from flask import Flask, g
 from .frontend import bp_frontend
 from .api import bp_api
+from os import environ, path
 from werkzeug.middleware.proxy_fix import ProxyFix
+from dotenv import load_dotenv
+
 
 app = Flask(
     __name__,
@@ -14,7 +17,23 @@ app = Flask(
     static_folder='static',
     instance_relative_config=False
 )
+
+if path.exists(".env"):
+    load_dotenv('.env', verbose=True)
+    app.logger.debug("Loaded .env")
+
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 app.config.from_object('config.Config')
 app.register_blueprint(bp_frontend)
 app.register_blueprint(bp_api, url_prefix='/api')
+
+
+@app.before_request
+def app_before_request():
+    """Set up g variables that will be useful for all requests."""
+    for key in environ.keys():
+        if not key.startswith('FLASK_G_'):
+            continue
+        _key = key.lower().replace('flask_g_', '')
+        setattr(g, _key, environ.get(key))
+        app.logger.debug(f"Setting g.{_key}={g.get(_key)}")
