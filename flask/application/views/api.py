a0b6bceaba46d6038ff6f3ab122d5778d689d360
Trey Allen <trey.allen@gmail.com>
2022-03-07 13:03:17 -0500

    Little refactoring to this after last project

--- flask/application/views/api.py
+++ flask/application/views/api.py
@@ -7,7 +7,7 @@ bp_api = Blueprint('api', __name__)
 @bp_api.route('/sup_world', methods=['GET'])
 def sup_world():
     """Return JSON from a database."""
-    from application.db import DB
+    from application.modules.db import DB
     db = DB()
     result = db.select_one("select * from skeleton")
     return jsonify(result)
